76391d82e840027a5cc8f4858f1d80452440491d
Federico Minaya <elfedeminaya@gmail.com>
2023-03-04 19:18:40 -0300

    fix: sign out refresh

--- examples/full-example/src/components/Buttons.tsx
+++ examples/full-example/src/components/Buttons.tsx
@@ -1,10 +1,19 @@
 'use client';
 import { signIn, signOut } from 'next-auth/react';
+import { useRouter } from 'next/navigation';
 
 export const SignInButton = () => {
   return <button onClick={() => signIn('github')}>Sign In</button>;
 };
 
 export const SignOutButton = () => {
-  return <button onClick={() => signOut({ redirect: false })}>Sign Out</button>;
+  const router = useRouter();
+
+  const handleClick = async () => {
+    await signOut({ redirect: false });
+
+    router.refresh();
+  };
+
+  return <button onClick={handleClick}>Sign Out</button>;
 };
